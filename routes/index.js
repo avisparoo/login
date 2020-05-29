8c0db429e344a3322e753148be1e2d6b46ba3b17
avisparoo <apoorva.bandikatte@gmail.com>
2020-05-29 20:16:16 +0530

    installed mongo, displays user list from collection users

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+
+//connect to mongodb
+mongoose.connect('mongodb+srv://apoorva:apoorva123@cluster0-xv4i2.mongodb.net/login?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+  });
+
+//to check mongdb connection
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    console.log(db.readyState);  
+  });
+
+//create schema and model
+  var userschema = new mongoose.Schema({
+    name: String,
+    email: String
+  });
+  var users = mongoose.model('users', userschema);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Users' });
+  //console.log(mongoose.connection.readyState);  //to check mongdb connection
+});
+
+router.get('/users', function (req, res, next) {
+  users.find({}, function (err, data) {
+    console.log(err, data, data.length);
+    data.forEach(element => {
+      console.log(element.name);
+      console.log(element.email);
+    });
+    res.json(data);
+    res.status(200);
+  });
 });
 
 module.exports = router;
